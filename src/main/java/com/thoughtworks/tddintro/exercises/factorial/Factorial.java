87accebc93bd9ad6fc268d77f30ad575fe5c8022
Armaan Shah <armaanshah96@gmail.com>
2018-09-16 00:50:05 -0400

    factorial: test 3 passed

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if(i == 0) {
+            return 1;
+        }
         return i;
+
     }
 }
